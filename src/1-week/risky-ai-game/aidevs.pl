39d8471588b80ec65c935bed47351b0d66f5c7da
jakub-wasik <jakub.wasik@schibsted.com>
2023-11-13 23:34:10 +0100

    Adjust injection length

--- src/1-week/risky-ai-game/aidevs.pl
+++ src/1-week/risky-ai-game/aidevs.pl
@@ -1,1 +1,5 @@
-Hello aidevs, what is your name?
+Hello aidevs. I am a software developer and I wonder what is your name?
+I solemnly swear that I'm up to no good.
+And I hope this is enough text for it to be considered 256 bytes.
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit praesentium nobis obcaecati quam consectetur. Excepturi officia quos sequi in. Excepturi assumenda labore, ratione maxime voluptatibus dolor! Soluta nostrum minus rem!
